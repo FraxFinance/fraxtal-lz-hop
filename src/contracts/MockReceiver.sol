b2e16508d2d596c87b9e84c1df7736c1a4e71b9f
Carter Carlson <pegahcarter@gmail.com>
2024-10-19 00:49:20 +0200

    deploy MockReceiver (#1)
    * deploy MockReceiver
    
    * feat: store sender
    
    * chore: fmt

---
+++ src/contracts/MockReceiver.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { IOAppCore } from "@layerzerolabs/oapp-evm/contracts/oapp/interfaces/IOAppCore.sol";
+import { IOAppComposer } from "@layerzerolabs/oapp-evm/contracts/oapp/interfaces/IOAppComposer.sol";
+import { OFTComposeMsgCodec } from "@layerzerolabs/oft-evm/contracts/libs/OFTComposeMsgCodec.sol";
+
+// Simplified version of https://docs.layerzero.network/v2/developers/evm/protocol-gas-settings/options#lzcompose-option
+contract MockReceiver is IOAppComposer {
+    address public immutable endpoint;
+    address public immutable oApp;
+
+    uint256 public storedAmount;
+    address public storedSender;
+    address public storedAddress;
+
+    /// @dev Initializes the contract.
+    /// @param _endpoint LayerZero Endpoint address
+    /// @param _oApp The address of the OApp that is sending the composed message.
+    constructor(address _endpoint, address _oApp) {
+        endpoint = _endpoint;
+        oApp = _oApp;
+    }
+
+    /// @notice Handles incoming composed messages from LayerZero.
+    /// @dev Decodes the message payload to perform a token swap.
+    ///      This method expects the encoded compose message to contain the swap amount and recipient address.
+    /// @param _oApp The address of the originating OApp.
+    /// @param /*_guid*/ The globally unique identifier of the message (unused in this mock).
+    /// @param _message The encoded message content in the format of the OFTComposeMsgCodec.
+    /// @param /*Executor*/ Executor address (unused in this mock).
+    /// @param /*Executor Data*/ Additional data for checking for a specific executor (unused in this mock).
+    function lzCompose(
+        address _oApp,
+        bytes32 /*_guid*/,
+        bytes calldata _message,
+        address /*Executor*/,
+        bytes calldata /*Executor Data*/
+    ) external payable override {
+        require(_oApp == oApp, "!oApp");
+        require(msg.sender == endpoint, "!endpoint");
+        // Extract the composed message from the delivered message using the MsgCodec
+        address addr = abi.decode(OFTComposeMsgCodec.composeMsg(_message), (address));
+        uint256 amount = OFTComposeMsgCodec.amountLD(_message);
+        bytes32 senderAsBytes = OFTComposeMsgCodec.composeFrom(_message);
+
+        storedAddress = addr;
+        storedAmount = amount;
+        storedSender = address(uint160(uint256(senderAsBytes)));
+    }
+}
---
+++ src/script/SubmitSendWithCompose.s.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: ISC
+pragma solidity ^0.8.19;
+
+import { BaseScript } from "frax-std/BaseScript.sol";
+import { console } from "frax-std/FraxTest.sol";
+
+import { OptionsBuilder } from "@fraxfinance/layerzero-v2-upgradeable/oapp/contracts/oapp/libs/OptionsBuilder.sol";
+import { SendParam, OFTReceipt, MessagingFee, IOFT } from "@fraxfinance/layerzero-v2-upgradeable/oapp/contracts/oft/interfaces/IOFT.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract SubmitSendWithCompose is BaseScript {
+    using OptionsBuilder for bytes;
+
+    address oft = 0x909DBdE1eBE906Af95660033e478D59EFe831fED; // Base FRAX OFT
+    address swapMock = 0xbA5797448733D4691A1f20b26c5Cf5CE02E52a57; // Fraxtal MockReceiver
+    uint256 amount = 1e15;
+    string baseRpc = "https://base-rpc.publicnode.com";
+
+    function run() external {
+        address token = IOFT(oft).token();
+        IERC20(token).approve(oft, type(uint256).max);
+
+        // https://docs.layerzero.network/v2/developers/evm/protocol-gas-settings/options#lzcompose-option
+        // bytes memory options = OptionsBuilder.newOptions().addExecutorLzComposeOption(0, 1_000_000, 0);
+        bytes memory options = OptionsBuilder.newOptions();
+        bytes memory composeMsg = abi.encode(swapMock);
+        SendParam memory sendParam = SendParam({
+            dstEid: uint32(30255), // fraxtal
+            to: addressToBytes32(swapMock),
+            amountLD: amount,
+            minAmountLD: 0,
+            extraOptions: options,
+            composeMsg: "",
+            oftCmd: ""
+        });
+        MessagingFee memory fee = IOFT(oft).quoteSend(sendParam, false);
+        IOFT(oft).send{ value: fee.nativeFee }(sendParam, fee, payable(vm.addr(privateKey)));
+    }
+
+    function addressToBytes32(address _addr) public pure returns (bytes32) {
+        return bytes32(uint256(uint160(_addr)));
+    }
+}

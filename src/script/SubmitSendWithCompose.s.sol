18aaa3154b6f7e09bf9122f7dc452d6149284240
Carter Carlson <pegahcarter@gmail.com>
2024-10-28 20:24:37 +0100

    feat: unwrap frxEth (#3)
    * feat: unwrap frxEth
    
    * fix: outdated references
    
    * fix: receive ether
    
    * chore: fmt
    
    * chore: rm counter
    
    * chore: fmt
    
    * chore(build): comment out test

--- src/script/SubmitSendWithCompose.s.sol
+++ src/script/SubmitSendWithCompose.s.sol
@@ -11,9 +11,10 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract SubmitSendWithCompose is BaseScript {
     using OptionsBuilder for bytes;
 
-    address oft = 0x909DBdE1eBE906Af95660033e478D59EFe831fED; // Base FRAX OFT
-    address swapMock = 0xEdC5087033c8C0561919E6eD7DCA32b40538929f; // Fraxtal MockReceiver
-    uint256 amount = 1e15;
+    // address oft = 0x909DBdE1eBE906Af95660033e478D59EFe831fED; // Base FRAX OFT
+    address oft = 0xF010a7c8877043681D59AD125EbF575633505942; // Base frxETH OFT
+    address swapMock = 0x60356998558A466Ec51BdE7e78F3b88Bdc843c5e; // Fraxtal MockReceiver
+    uint256 amount = 1e13;
     string baseRpc = "https://base-rpc.publicnode.com";
 
     function run() external broadcaster {
@@ -21,7 +22,7 @@ contract SubmitSendWithCompose is BaseScript {
         bytes memory options = OptionsBuilder.newOptions().addExecutorLzComposeOption(0, 1_000_000, 0);
         // bytes memory options = OptionsBuilder.newOptions();
         /// @dev: fails when second argument too high
-        bytes memory composeMsg = abi.encode(0xb0E1650A9760e0f383174af042091fc544b8356f, uint256(1e16));
+        bytes memory composeMsg = abi.encode(0xb0E1650A9760e0f383174af042091fc544b8356f, uint256(0));
         SendParam memory sendParam = SendParam({
             dstEid: uint32(30255), // fraxtal
             to: addressToBytes32(swapMock),

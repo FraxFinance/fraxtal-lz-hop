d4d1786c55271c404277735f9fabf274787f28cf
Carter Carlson <pegahcarter@gmail.com>
2024-10-28 11:55:50 -0700

    chore: rm counter

--- src/script/DeployMockReceiver.s.sol
+++ src/script/DeployMockReceiver.s.sol
@@ -5,7 +5,7 @@ import { BaseScript } from "frax-std/BaseScript.sol";
 import { console } from "frax-std/FraxTest.sol";
 import { MockReceiver } from "../contracts/MockReceiver.sol";
 
-// Run this with source .env && forge script --broadcast --rpc-url $MAINNET_URL DeployCounter.s.sol
+// Run this with source .env && forge script --broadcast --rpc-url $MAINNET_URL DeployMockReceiver.s.sol
 contract DeployMockReceiver is BaseScript {
     address endpoint = 0x1a44076050125825900e736c501f859c50fE728c; // fraxtal endpoint v2
 

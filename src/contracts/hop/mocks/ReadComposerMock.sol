713ac1d5e986ae1cbef66680f202261db29afab5
Carter Carlson <pegahcarter@gmail.com>
2025-11-10 12:12:18 -0800

    feat: init mock

---
+++ src/contracts/hop/mocks/ReadComposerMock.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.8.0;
+
+import { IReadComposer } from "src/contracts/hop/interfaces/IReadComposer.sol";
+
+contract ReadComposerMock is IReadComposer {
+
+    // decoded nonce fields
+    bool public deposit;
+    uint64 public amount;
+    address public recipient;
+
+    // readCompose params
+    uint32 public srcEid;
+    bytes32 public srcAddress;
+    uint256 public nonce;
+    uint64 public readTimestamp;
+    bool public success;
+    bytes public data; 
+
+    function readCompose(
+        uint32 _srcEid,
+        bytes32 _srcAddress,
+        uint256 _nonce,
+        uint64 _readTimestamp,
+        bool _success,
+        bytes memory _data
+    ) external override {
+        srcEid = _srcEid;
+        srcAddress = _srcAddress;
+        nonce = _nonce;
+        readTimestamp = _readTimestamp;
+        success = _success;
+        data = _data;
+
+        // decode nonce
+        bytes memory nonceAsBytes = abi.encodePacked(_nonce);
+        uint32 depositAsUint;
+        uint64 amountCached;
+
+        // https://github.com/GNSPS/solidity-bytes-utils/blob/fc502455bb2a7e26a743378df042612dd50d1eb9/contracts/BytesLib.sol#L334-L354
+        assembly {
+            amountCached := mload(add(add(nonceAsBytes, 0x8), 0))
+            depositAsUint := mload(add(add(nonceAsBytes, 0x4), 0x8))
+        }
+
+        amount = amountCached;
+        deposit = depositAsUint == 1;
+        recipient = address(uint160(nonce));
+    }
+}
